24d2ffc4cfb0146debd167abe92ff7e80d361f6f
Riley <rptallman@gmail.com>
2020-01-29 12:48:28 -0700

    beginning project_2a

---
+++ project_2a/code/task_1/task1.py
@@ -0,0 +1,13 @@
+#!usr/bin/python
+
+import cv2
+import glob
+
+if __name__ == '__main__':      # if we are running this file directly
+    # STEP 1: read in all images to memory
+    left_ = [cv2.imread(image) for image in glob.glob("../../images/task_1/left_*.png")]
+    right = [cv2.imread(image) for image in glob.glob("../../images/task_1/right_*.png")]
+
+    # STEP 2: Extract 3D to 2D point correspondences
+    
+    
