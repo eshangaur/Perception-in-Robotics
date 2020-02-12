f741aec1cc525657d5f0f3a736e1143b1d17ed50
Riley Tallman <rptallman@gmail.com>
2020-02-12 09:33:12 -0700

    the bot is moving :)

--- project_3a/pursuit_evasion_ws/src/pursuit_evasion_pkg/scripts/traverse_floor.py
+++ project_3a/pursuit_evasion_ws/src/pursuit_evasion_pkg/scripts/traverse_floor.py
@@ -10,48 +10,17 @@
     messages on the topic 'turtle1/cmd_vel' to draw the 'T'.
 """
 
-import rospy
-from geometry_msgs.msg import Twist, Vector3
+from turtlebot3_controller import turtlebot3_controller
 
 
-def drawT():
-    rospy.init_node('traverse_floor', anonymous=True)   # start a new node with name 'my_initials'
-    publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=30)  # publisher object
-    rate = rospy.Rate(100) # 1000hz
-    # rospy.sleep(1)
+def moveAround(controller):
 
-    if rospy.is_shutdown():     # make sure ros is running
-        import sys
-        sys.exit()  # exit program.
+    controller.goToPoint(-5,0,0)
 
-    rate.sleep()
-    startTime = rospy.get_time()
-    log = "Commence drawing %s" % startTime
-    rospy.loginfo(log)      # log the time
-
-    while(rospy.get_time() < (startTime + 3.0)):      # for 3 seconds
-        publisher.publish(Twist(Vector3(.5,0,0),Vector3(0,0,0)))        # move forward
-        rate.sleep()
-
-    # twist = Twist(Vector3(.3,0,0),Vector3(0,0,0))    # start with moving forward
-
-    # while(not rospy.is_shutdown()):     # repeatedly publish whatever direction and angle we want, depending on the time
-
-
-    #     if(rospy.get_time() > 5):
-    #         twist = Twist(Vector3(.3,0,0),Vector3(0,0,.1))
-
-    #     publisher.publish(twist)
-
-    # publisher.publish(Twist(Vector3(.2,0,0),Vector3(0,0,0)))        # move forward
-    # rospy.sleep(2)
-
-    # publisher.publish(Twist(Vector3(0,0,0),Vector3(0,0,0)))        # stop
-    
 
 
 if __name__ == '__main__':
-    try:
-        drawT()
-    except rospy.ROSInterruptException:
-        pass
+    controller = turtlebot3_controller()
+    moveAround(controller)
+    controller.shutdown()
+
