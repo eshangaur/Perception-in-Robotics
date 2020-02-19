f7b5611579168003a27e9c60b7a62e5ef74a2329
Riley Tallman <rptallman@gmail.com>
2020-02-19 13:55:29 -0700

    finished project_3a

--- project_3a/pursuit_evasion_ws/src/pursuit_evasion_pkg/scripts/traverse_floor.py
+++ project_3a/pursuit_evasion_ws/src/pursuit_evasion_pkg/scripts/traverse_floor.py
@@ -5,8 +5,8 @@
     CSE 598 Perception in Robotics
     1/23/2020
     Description: This is a ROS node written in python that instructs
-    the turtlesim_node from the turtlesim package to draw a capital 
-    'T' to the screen. It initializes a node and then publishes Twist
+    the turtlebot3 to draw a 'T' to the screen. After moving to a 
+    separate room. It initializes a node and then publishes Twist
     messages on the topic 'turtle1/cmd_vel' to draw the 'T'.
 """
 
@@ -16,19 +16,15 @@ from turtlebot3_controller import turtlebot3_controller
 def moveAround(controller):
 
     # 90 points towards +y 
-    controller.goToPoint(5,-3,-180)       # gets to the point
-    controller.goToPoint(0,-2,-90)          # success!
-    controller.goToPoint(1,0,0)
-    controller.goToPoint(4.1,0,80)         # enter the room with a walking guy
-    controller.goToPoint(5,4,-45)
-    controller.goToPoint(6,3,-90)        # starts to move in a figure 8
-    controller.goToPoint(6,0,180)
-    controller.goToPoint(-4,0,0)
-    controller.goToPoint(-1,0,90)
-    controller.goToPoint(-1,5,0)
-    controller.goToPoint(1,5,90)
-    controller.goToPoint(2,5,-90)
-    controller.goToPoint(0,5,-90)
+    controller.goToPoint(0,-2,90)
+    controller.goToPoint(0,0,-180)
+    controller.goToPoint(-3,0,90)
+    controller.goToPoint(-3,3,-180)
+
+    # draw a 'T'
+    controller.goToPoint(-6,3,90)
+    controller.goToPoint(-6,6,-90)
+    controller.goToPoint(-6,0,-90)
 
 
 
